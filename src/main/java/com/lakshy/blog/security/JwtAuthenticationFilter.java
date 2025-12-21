183e2a4673e921edc542f8f89e0e6889611777fe
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2025-12-21 18:21:11 +0300

    fix the logs null jwt for helth

--- src/main/java/com/lakshy/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/lakshy/blog/security/JwtAuthenticationFilter.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.ExpiredJwtException;
@@ -27,6 +29,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
 	@Autowired
 	private JwtTokenHelper jwtTokenHelper;
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 	
 	// this is called when we hit any api request
 	@Override
@@ -35,8 +39,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		
 		// 1. get token from request from Authorization key in header
 		String requestToken = request.getHeader("Authorization");
-		
-		System.out.println(requestToken);
+		String requestPath = request.getRequestURI();
+
+		logger.trace("Request URI: {} Authorization header: {}", requestPath, requestToken);
 		
 		String username = null;
 		String token = null;
@@ -46,22 +51,25 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			token = requestToken.substring(7); // after bearer fetch original token
 			try {
 				username = this.jwtTokenHelper.getUsernameFromToken(token);
-				
 			}
 			catch(IllegalArgumentException ex){
-				System.out.println("Unable to get JWT Token");
+				logger.debug("Unable to get JWT Token: {}", ex.getMessage());
 			}
 			catch(ExpiredJwtException ex) {
-				System.out.println("JWT Token has expired");
+				logger.debug("JWT Token has expired: {}", ex.getMessage());
 			}
 			catch(MalformedJwtException ex) {
-				System.out.println("Invalid JWT");
+				logger.debug("Invalid JWT: {}", ex.getMessage());
 			}
 			
 		}
-		else 
-		{
-			System.out.println("JWT Token is null or doesnot begin with bearer");
+		else {
+			// Don't spam logs for health checks or other public endpoints
+			if (!requestPath.contains("/health")) {
+				logger.debug("JWT Token is null or does not begin with Bearer");
+			} else {
+				logger.trace("Skipping JWT header warning for health check request");
+			}
 		}
 		
 		// 2. we got the JWT token, Now validate the JWT Token
@@ -82,13 +90,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 				
 			} else {
 				// validation of token failed
-				System.out.println("Invalid JWT Token");
+				logger.debug("Invalid JWT Token");
 			}
 			
-		}else {
+		} else {
 			// a security is already being defined in current application (basic db security or some other)
 			// or the username is null for this JWT token
-			System.out.println("username is null or context is not null");
+			logger.trace("username is null or context is not null");
 		}
 		
 		// if jwt token is valid then above conditions might have set the authentication context to authorize the api
