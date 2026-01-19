8c06cab90f5f6edf36c0af9bbffee8b7c48bb62c
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2026-01-19 18:14:12 +0300

    filtering the logs print

---
+++ src/main/java/com/lakshy/blog/config/RequestLoggingFilter.java
@@ -0,0 +1,23 @@
+package com.lakshy.blog.config;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        if (!"/api/v1/health".equals(uri)) {
+            System.out.println(method + " " + uri);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
